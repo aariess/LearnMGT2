fa00274be61d537db3b2ab23e9b774ce5df768fb
linhnt6677 <linhnt6677@co-well.com.vn>
2018-07-04 12:53:41 +0700

    lession 1

--- Lession 1/scripts/appViewModel.js
+++ Lession 1/scripts/appViewModel.js
@@ -1,7 +1,7 @@
 // Main viewmodel class
 define(['knockout-3.5.0rc'], function(ko) {
     return function appViewModel() {
-        this.name = ko.observable('');
+        this.name = ko.observable('Linh');
         this.nameCaps = ko.pureComputed(function() {
             return this.name();
         }, this);
